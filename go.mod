65371cc7dd3c5558be58a2d9d0b8dcebc509c9b2
vyas-git <vyasreddy.tech@gmail.com>
2021-08-19 18:59:15 +0530

    update main files

---
+++ go.mod
@@ -0,0 +1,11 @@
+module goweb-examples
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	ex "goweb-examples/examples"
+)
+
+func main() {
+	//ex.Helloworld()
+	//ex.CustomRouter()
+	//ex.Viewtemplate()
+
+	//obj := ex.SingleMiddleware{}
+
+	//obj := ex.MultiMiddleware{}
+
+	//obj := ex.AuthSession{}
+
+	//obj := ex.JsonImplentation{}
+
+	//obj := ex.SocketsImplementation{}
+
+	obj := ex.PasswordHashing{}
+
+	obj.Run()
+}
